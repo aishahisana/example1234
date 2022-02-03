2b7f7879606cd97b89902c7d4c78afb84ea01974
aishahisana <aishahisana119@gmail.com>
2022-02-02 16:08:33 -0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    has
+    <a href="{{url('/')}}">home</a>
+    <a href="{{url('about')}}">about</a>
+    <img src="{{asset('images1/java.jpg')}}" height="200" width="200">
+    <img src="{{asset('images1/php.png')}}" height="200" width="200">
+
+</body>
+</html>
+
